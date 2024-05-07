5f6710cdbd27f477e43e9f5cadee61d2e0105adf
yu-takahashi <yuta.takahashi1996@gmail.com>
2024-05-07 14:34:51 +0000

    feat: APIメッセージの定義を追加

---
+++ src/config/apiMessages.ts
@@ -0,0 +1,4 @@
+export const SUCCESS = 'success'
+export const ERROR_USER_NOT_FOUND = 'User not found'
+export const ERROR_AUTH_FAIL = 'Authentication failure'
+export const ERROR_JWT_EXP_ERROR = 'expiration error'
