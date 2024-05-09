afb2202f23ba65f5bb80e2fd8070dce914512a5c
yu-takahashi <yuta.takahashi1996@gmail.com>
2024-05-09 00:06:29 +0000

    feat: 管理画面サイドメニューの定義を追加

--- src/config/constantText.ts
+++ src/config/constantText.ts
@@ -1,1 +1,28 @@
 export const SITE_TITLE = 'Example Blog Title'
+export type ADMIN_MENUE = {
+  id: number
+  name: string
+  href: string
+}
+export const ADMIN_SIDE_MENUE = [
+  {
+    id: 1,
+    name: 'サマリー',
+    href: '/admin/summary',
+  } as ADMIN_MENUE,
+  {
+    id: 2,
+    name: '記事一覧',
+    href: '/admin/articles',
+  } as ADMIN_MENUE,
+  {
+    id: 3,
+    name: '記事追加',
+    href: '/admin/create-article',
+  } as ADMIN_MENUE,
+  {
+    id: 4,
+    name: '固定ページ編集',
+    href: '/admin/fixed-page-edit',
+  } as ADMIN_MENUE,
+]
