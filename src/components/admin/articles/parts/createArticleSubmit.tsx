1820c7675d9337a942018846bdb92dea6f0b5d69
yu-takahashi <yuta.takahashi1996@gmail.com>
2024-07-21 14:48:49 +0000

    fix: 簡易的なバリデーションを追加

--- src/components/admin/articles/parts/createArticleSubmit.tsx
+++ src/components/admin/articles/parts/createArticleSubmit.tsx
@@ -8,6 +8,7 @@ import "easymde/dist/easymde.min.css"
 import { gql, useMutation } from "@apollo/client"
 import { useRecoilValue } from "recoil"
 import { myJwtState } from "@/state/jwtState"
+import { useRouter } from "next/navigation"
 
 type props = {
   articleTitle: string
@@ -22,12 +23,7 @@ export const CreateArticleSubmit: React.FC<props> = ({
   articleImages,
   articlaCategoryId,
 }) => {
-  const [articleData, setArticleData] = useState("")
-
-  const onArticleDataChange = useCallback((value: string) => {
-    setArticleData(value)
-  }, [])
-
+  const router = useRouter()
   const myJwt = useRecoilValue(myJwtState)
 
   const CREATE_ARTICLE_QUERY = gql`
@@ -53,26 +49,56 @@ export const CreateArticleSubmit: React.FC<props> = ({
   const [createArticle, { data, loading, error }] =
     useMutation(CREATE_ARTICLE_QUERY)
 
+  const [validMsg, setValidMsg] = useState<string[]>([])
+
+  const articleValidation = () => {
+    const tmpMsg = []
+    if (!articleTitle || articleTitle === "") {
+      tmpMsg.push("記事タイトルは必須項目です")
+    }
+    if (!articleBody || articleBody === "") {
+      tmpMsg.push("記事本文は必須項目です")
+    }
+    if (!articlaCategoryId || articlaCategoryId === "") {
+      tmpMsg.push("記事カテゴリーは必須項目です")
+    }
+
+    setValidMsg(tmpMsg)
+    return tmpMsg.length === 0
+  }
+
   const handleCreateArticle = () => {
-    createArticle({
-      variables: {
-        jwt: myJwt,
-        articleBody: articleBody,
-        articleTitle: articleTitle,
-        articleImages: articleImages,
-        categoryId: parseInt(articlaCategoryId),
-      },
-    }).then((data) => {
-      console.log(data)
-    })
+    if (articleValidation()) {
+      createArticle({
+        variables: {
+          jwt: myJwt,
+          articleBody: articleBody,
+          articleTitle: articleTitle,
+          articleImages: articleImages,
+          categoryId: parseInt(articlaCategoryId),
+        },
+      }).then((data) => {
+        setValidMsg([])
+        router.push("/admin/articles")
+      })
+    }
   }
 
   return (
-    <Button
-      className=" bg-lime-500 ml-5 text-orange-900 font-bold hover:bg-orange-950"
-      onClick={handleCreateArticle}
-    >
-      記事を投稿する
-    </Button>
+    <>
+      <Button
+        className=" bg-lime-500 ml-5 text-orange-900 font-bold hover:bg-orange-950"
+        onClick={handleCreateArticle}
+      >
+        記事を投稿する
+      </Button>
+      {validMsg.length > 0 && (
+        <ul className=" ml-5 text-red-600 text-lg font-bold">
+          {validMsg.map((msg, index) => (
+            <li key={index}>{msg}</li>
+          ))}
+        </ul>
+      )}
+    </>
   )
 }
